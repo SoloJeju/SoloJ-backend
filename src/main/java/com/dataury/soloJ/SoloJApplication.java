8a6c4dfccd92584e91023406affc939c7f95f133
haeun <haeun9634@naver.com>
2025-06-25 22:59:46 +0900

    chore: 초기 프로젝트 구조 설정 및 기본 환경 구성

---
+++ src/main/java/com/dataury/soloJ/SoloJApplication.java
@@ -0,0 +1,13 @@
+package com.dataury.soloJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataury/soloJ/SoloJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataury.soloJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
