7c3f076c264726546d059e583759da42392891c8
haeun <haeun9634@naver.com>
2025-07-10 16:11:21 +0900

    fix: 순환 의존 해결 위해 생성자 주입 -> 메서드 주입으로 변경

--- src/main/java/com/dataury/soloJ/global/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/dataury/soloJ/global/security/oauth/CustomOAuth2UserService.java
@@ -4,7 +4,7 @@ import com.dataury.soloJ.domain.user.entity.User;
 import com.dataury.soloJ.domain.user.entity.status.Role;
 import com.dataury.soloJ.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.internal.util.stereotypes.Lazy;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
